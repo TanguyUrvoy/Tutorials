46c81167e0150e645ec126aa8d9a9fe3f9d7623a
Tanguy Urvoy <tanguy.urvoy@orange.com>
2021-02-17 11:43:47 +0100

    Update BuildBoost.cmake
    It took me some time to guess that boost libraries are now named boost_python3X and boost_numpy3X
    Unlike the original version, this patched version worked with my python 3.7.9.

--- PythonC++/cmake/Modules/BuildBoost.cmake
+++ PythonC++/cmake/Modules/BuildBoost.cmake
@@ -2,12 +2,15 @@
 # Build the Boost dependencies for the project using a specific version of python #
 #========================================================#
 
-set(BoostVersion 1.63.0)
-set(BoostMD5 1c837ecd990bb022d07e7aab32b09847)
+set(BoostVersion 1.75.0)
+set(BoostMD5 ea217ed7c4414e93d44106c316966ae1)
+
 
 string(REGEX REPLACE "beta\\.([0-9])$" "beta\\1" BoostFolderName ${BoostVersion})
 string(REPLACE "." "_" BoostFolderName ${BoostFolderName})
 set(BoostFolderName boost_${BoostFolderName})
+message(STATUS "BoostFolderName= ${BoostFolderName}")
+message(STATUS "Boost URL = http://sourceforge.net/projects/boost/files/boost/${BoostVersion}/${BoostFolderName}.tar.bz2/download")
 
 ExternalProject_Add(Boost
     PREFIX Boost
@@ -30,9 +33,14 @@ ExternalProject_Add(Boost
 set(Boost_LIBRARY_DIR ${CMAKE_BINARY_DIR}/extern/lib/ )
 set(Boost_INCLUDE_DIR ${CMAKE_BINARY_DIR}/extern/include/boost/ )
 
+message(STATUS "PYTHON_VERSION_STRING = ${PYTHON_VERSION_STRING}")
+string(REGEX REPLACE ".[0-9]$" "" PYTHON_VER ${PYTHON_VERSION_STRING})
+string(REPLACE "." "" PYTHON_VER ${PYTHON_VER})
+message(STATUS "PYTHON_VER = ${PYTHON_VER}")
+
 if(${PYTHON_VERSION_STRING} GREATER 3.0)
-  message(STATUS "Using Python3")
-  set(Boost_LIBRARIES -lboost_python3 -lboost_numpy)
+  message(STATUS "Using Python${PYTHON_VER}")
+  set(Boost_LIBRARIES -lboost_python${PYTHON_VER} -lboost_numpy${PYTHON_VER})
 else()
   message(STATUS "Using Python2")
   set(Boost_LIBRARIES -lboost_python -lboost_numpy)
